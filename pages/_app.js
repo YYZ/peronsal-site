bd54520eae9f508b004b47ee3aa4191f45f17993
sam <samhalligan15@gmail.com>
2024-04-25 17:49:37 +0200

    add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,29 @@
 import '../styles/custom.global.scss'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
 
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-FQCXGRHLMR"
+      ></script>
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FQCXGRHLMR');
+          `,
+        }}
+      />
+    </>
+  );
+}
 export default MyApp
